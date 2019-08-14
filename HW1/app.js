e4c7d68e308f77cd143a35d90ba31d5934629f1d
JossinAntony <jossin.antony@causalitybiomodels.com>
2019-08-14 20:57:12 +0530

    Login and register pages connected with app.

--- HW1/app.js
+++ HW1/app.js
@@ -4,8 +4,12 @@ var app = new Express();                                                    //2.
 
 app.set('view engine','ejs');                                               //5. set up view engine
 
-app.get('/', (req,res)=>{
-    res.send("<h1>index</h1");
+app.get('/login', (req,res)=>{
+    res.render('login');
+});
+
+app.get('/register', (req,res)=>{
+    res.render('register');
 });
 
 app.listen(3000,()=>{                                                       //3. set up listener
